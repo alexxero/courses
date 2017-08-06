b4588a2edf7d8f5cdf3aa05c5898d0b75a03814b
alexxero <alexxero@mail.ru>
2017-08-06 14:50:48 +0300

    Appoint courses to groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -69,6 +69,7 @@ class GroupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
-      params.require(:group).permit(:name)
+      params.require(:group).permit!
+      # params.require(:group).permit(:name)
     end
 end
