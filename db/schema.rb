c388a42036d3407035457e2502ba9ac1b659753c
raleksey <ruby@mosplitka.ru>
2017-08-07 11:43:27 +0300

    Appoint courses to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806183017) do
+ActiveRecord::Schema.define(version: 20170807081906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20170806183017) do
   create_table "courses_groups", id: false, force: :cascade do |t|
     t.integer "course_id"
     t.integer "group_id"
+    t.integer "user_id"
+  end
+
+  create_table "courses_users", id: false, force: :cascade do |t|
+    t.integer "course_id"
     t.integer "user_id"
   end
 
